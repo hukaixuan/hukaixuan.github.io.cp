7258a391b86cb608b409b8803cc6297517fd53c7
hukx.michael <1782964880@qq.com>
2018-04-04 15:59:34 +0800

    Site updated: 2018-04-04 15:59:34

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-(function($){
+(function ($) {
   console.log('© Theme-Vexo | https://github.com/yanm1ng/hexo-theme-vexo')
   var app = $('.app-body')
   var header = $('.header')
   var banner = document.getElementById('article-banner') || false
   var about = document.getElementById('about-banner') || false
   var top = $('.scroll-top')
-  var path = window.location.pathname
+  var catalog = $('.catalog-container .toc-main')
   var isOpen = false
 
-  $(document).ready(function() {
-    NProgress.start();
+  $(document).ready(function () {
+    NProgress.start()
     $('#nprogress .bar').css({
       'background': '#42b983'
-    });
+    })
     $('#nprogress .spinner').hide()
-    
+
     var fade = {
       transform: 'translateY(0)',
       opacity: 1
@@ -29,13 +29,13 @@
     app.css(fade)
   })
 
-  window.onload = function() {
-    setTimeout(function() {
-      NProgress.done();
+  window.onload = function () {
+    setTimeout(function () {
+      NProgress.done()
     }, 200)
-  };
+  }
 
-  $('.menu').on('click', function() {
+  $('.menu').on('click', function () {
     if (!header.hasClass('fixed-header') || isOpen) {
       header.toggleClass('fixed-header')
       isOpen = !isOpen
@@ -43,33 +43,49 @@
     $('.menu-mask').toggleClass('open')
   })
 
-  $('#tag-cloud a').on('click', function() {
+  $('#tag-cloud a').on('click', function () {
     var list = $('.tag-list')
     var name = $(this).data('name')
     var maoH = list.find('#' + name).offset().top
 
-    $('html,body').animate({ scrollTop: maoH - header.height() }, 500);
+    $('html,body').animate({
+      scrollTop: maoH - header.height()
+    }, 500)
   })
 
-  $('.reward-btn').on('click', function() {
+  $('.reward-btn').on('click', function () {
     $('.money-code').fadeToggle()
   })
 
-  $('.arrow-down').on('click', function() {
-    $('html,body').animate({ scrollTop: banner.offsetHeight - header.height() }, 500);
+  $('.arrow-down').on('click', function () {
+    $('html, body').animate({
+      scrollTop: banner.offsetHeight - header.height()
+    }, 500)
   })
 
-  top.on('click', function() {
-    $('html,body').animate({ scrollTop: 0 }, 600);
+  $('.toc-nav a').on('click', function (e) {
+    e.preventDefault()
+    var catalogTarget = e.currentTarget
+    var scrollTarget = $(catalogTarget.getAttribute('href'))
+    var top = scrollTarget.offset().top
+    if (top > 0) {
+      $('html,body').animate({
+        scrollTop: top - 65
+      }, 500)
+    }
   })
 
-  document.addEventListener('scroll', function() {
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  top.on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+  })
+
+  document.addEventListener('scroll', function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
     var headerH = header.height()
     if (banner) {
       if (scrollTop > headerH) {
         header.addClass('fixed-header')
-      } else if (scrollTop == 0){
+      } else if (scrollTop === 0) {
         header.removeClass('fixed-header')
       }
     }
@@ -78,6 +94,10 @@
     } else {
       top.removeClass('opacity')
     }
-  });
-
-})(jQuery);
+    if (scrollTop > 190) {
+      catalog.addClass('fixed-toc')
+    } else {
+      catalog.removeClass('fixed-toc')
+    }
+  })
+})(jQuery)
